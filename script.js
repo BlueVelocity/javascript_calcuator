d5ed14894b1b59fe793c54690c25c4e484c7559c
joseph <josephrodney@hotmail.com>
2023-07-08 00:10:50 -0400

    Implement javascript for display functionality
    Made changes to the HTML and CSS file. Now using a string as output.
    
    Added javascript functionality to represent input in the display

---
+++ script.js
@@ -0,0 +1,9 @@
+displayElement = document.getElementById('')
+inputArrayString = null
+
+inputDisplay = '';
+
+function display(input) {
+    inputDisplay += input
+    document.getElementById('displayOutput').innerHTML = inputDisplay;
+}
