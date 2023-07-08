bee238f3444edbedaba7ea86f58f68a9923b859f
joseph <josephrodney@hotmail.com>
2023-07-08 02:15:38 -0400

    Allow for single use calculation
    Javascript has been added to allow for simple operator function
    that will print to the calculator display.
    Currently, unable to clear the display field

--- script.js
+++ script.js
@@ -1,18 +1,77 @@
 let displayElement = document.getElementById('')
-let inputArrayString = null;
 let operatorToggle = true;
+let numberOne = '';
+let numberTwo = '';
+let operator = '';
+let resume = '';
 
-let inputDisplay = '';
+inputDisplay = '';
 
 function display(input) {
-    inputDisplay += input
+    inputDisplay += input;
     document.getElementById('displayOutput').innerHTML = inputDisplay;
 }
 
 function displayOperator(input) {
-    if (operatorToggle === true) {
+    if (operatorToggle == true) {
         inputDisplay += input;
         document.getElementById('displayOutput').innerHTML = inputDisplay;
         operatorToggle = false;
     } 
+}
+
+function equals() {
+    inputArray = inputDisplay.split('');
+    for (i = 0; i < inputArray.length; i++) {
+        if (/\d+/.test(inputArray[i])) {
+            numberOne += inputArray[i];
+        } else {
+            resume = i + 1;
+            operator = inputArray[i];
+            break;
+        }
+    }
+
+    for (i = resume; i < inputArray.length; i++) {
+        numberTwo += inputArray[i];
+    }
+
+    switch(operator) {
+        case '+' :additionPrintToDisplay(numberOne, numberTwo);
+            break;
+        case '-' :subtractionPrintToDisplay(numberOne, numberTwo);
+            break;
+        case '*' :multiplicationPrintToDisplay(numberOne, numberTwo);
+            break;
+        case '/' :divisionPrintToDisplay(numberOne, numberTwo);
+            break;
+    }
+    console.log(numberOne);
+    console.log(resume);
+    console.log(numberTwo);
+    console.log(operator);
+}
+
+function additionPrintToDisplay(num1, num2) {
+    let answer = undefined;
+    answer = num1 + num2;
+    document.getElementById('displayOutput').innerHTML = answer;
+}
+
+function subtractionPrintToDisplay(num1, num2) {
+    let answer = undefined;
+    answer = num1 - num2;
+    document.getElementById('displayOutput').innerHTML = answer;
+}
+
+function multiplicationPrintToDisplay(num1, num2) {
+    let answer = undefined;
+    answer = num1 * num2;
+    document.getElementById('displayOutput').innerHTML = answer;
+}
+
+function divisionPrintToDisplay(num1, num2) {
+    let answer = undefined;
+    answer = num1 / num2;
+    document.getElementById('displayOutput').innerHTML = answer;
 }
